0aab370dcc2f20d2d00a93fcd71d75721fbab151
Enes Anbar <eanbar@ebay.com>
2018-10-21 01:54:18 +0300

    Implement Map based services

---
+++ pet-clinic-data/src/main/java/com/enesanbar/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package com.enesanbar.petclinic.services.map;
+
+import com.enesanbar.petclinic.models.Owner;
+import com.enesanbar.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+}
