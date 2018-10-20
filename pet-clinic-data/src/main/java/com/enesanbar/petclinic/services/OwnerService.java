4df1d030baa4f4cdb1931084791a0fc0b8488d66
Enes Anbar <eanbar@ebay.com>
2018-10-21 01:39:12 +0300

    Refactor service interfaces into base interface

--- pet-clinic-data/src/main/java/com/enesanbar/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/enesanbar/petclinic/services/OwnerService.java
@@ -2,14 +2,5 @@ package com.enesanbar.petclinic.services;
 
 import com.enesanbar.petclinic.models.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
+public interface OwnerService extends CrudService<Owner, Long> {
 }
--- pet-clinic-data/src/main/java/com/enesanbar/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/enesanbar/petclinic/services/VetService.java
@@ -1,16 +1,6 @@
 package com.enesanbar.petclinic.services;
 
-import com.enesanbar.petclinic.models.Pet;
 import com.enesanbar.petclinic.models.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-    
-    Set<Vet> findAll();
-
+public interface VetService extends CrudService<Vet, Long> {
 }
