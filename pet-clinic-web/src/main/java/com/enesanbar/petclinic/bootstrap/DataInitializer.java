6296a9c16fe765c62c1c9c4954d6a08e0ddabf5a
Enes Anbar <eanbar@ebay.com>
2018-10-22 21:41:13 +0300

    Enhance Owners with Pets and Contact Info

--- pet-clinic-web/src/main/java/com/enesanbar/petclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/enesanbar/petclinic/bootstrap/DataInitializer.java
@@ -1,13 +1,17 @@
 package com.enesanbar.petclinic.bootstrap;
 
 import com.enesanbar.petclinic.models.Owner;
+import com.enesanbar.petclinic.models.Pet;
 import com.enesanbar.petclinic.models.PetType;
 import com.enesanbar.petclinic.models.Vet;
 import com.enesanbar.petclinic.services.OwnerService;
 import com.enesanbar.petclinic.services.PetTypeService;
 import com.enesanbar.petclinic.services.VetService;
+import org.apache.tomcat.jni.Local;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -36,12 +40,33 @@ public class DataInitializer implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
+        owner1.setAddress("Aydinlar Mah.");
+        owner1.setCity("Istanbul");
+        owner1.setTelephone("123456");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Enes");
         owner2.setLastName("Anbar");
+        owner2.setAddress("Kasimpasa");
+        owner2.setCity("Istanbul");
+        owner2.setTelephone("654321");
         ownerService.save(owner2);
+
+        Pet enesCat = new Pet();
+        enesCat.setPetType(savedCatType);
+        enesCat.setOwner(owner2);
+        enesCat.setBirthDate(LocalDate.now());
+        enesCat.setName("Just Cat");
+        owner2.getPets().add(enesCat);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("John");
