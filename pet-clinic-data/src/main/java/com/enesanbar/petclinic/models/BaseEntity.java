88d28d29db1f139bf6f33467b7c752f609d38c8e
Enes Anbar <eanbar@ebay.com>
2018-10-28 18:29:17 +0300

    Implement base entity

--- pet-clinic-data/src/main/java/com/enesanbar/petclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/enesanbar/petclinic/models/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.enesanbar.petclinic.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// don't map this class to db, because other entities inherit from this
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
